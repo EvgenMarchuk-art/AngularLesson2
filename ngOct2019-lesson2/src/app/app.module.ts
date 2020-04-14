59b1781a5872789d836c60ea3e5e47a3c77fccfc
homecomp <0966009853m@gmail.com>
2020-04-14 12:57:29 +0300

    Lesson2

---
+++ ngOct2019-lesson2/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {UserComponent} from './user/user.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HelloComponent} from './hello/hello.component';
+import {AllUsersComponent} from './all-users/all-users.component';
+import {UserResolverService} from './services/user-resolver.service';
+
+
+const routes: Routes = [
+  // locahost:4200/ -> hello component
+  {path: '', component: HelloComponent},
+  // localhos:4200/users ->all Users Cpmponent
+  {path: 'users', component: AllUsersComponent, resolve: {allUsers : UserResolverService}},
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    HelloComponent,
+    AllUsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
